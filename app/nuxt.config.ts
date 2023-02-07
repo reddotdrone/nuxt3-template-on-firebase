a2bcbcf4fc2433e31c1b251d32d222f62aa3e5e8
akirahrkw <akirahrkw@gmail.com>
2023-02-07 14:13:40 +0800

    update nuxt.config.ts

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
+
+  nitro: {
+    preset: 'firebase',
+    serveStatic: false,
+  },
 
   app: {
     head: {
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd app
-NITRO_PRESET=firebase npm run build
+npm run build
 npm run clean
 cd ..
